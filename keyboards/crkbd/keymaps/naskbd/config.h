f49075b6b1158ae69040a3476c0dc32936479a3e
tbizetde@gmail.com <tbizetde@gmail.com>
2025-10-09 03:03:56 -0300

    reduced tapping term

--- keyboards/crkbd/keymaps/naskbd/config.h
+++ keyboards/crkbd/keymaps/naskbd/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 
 #define QUICK_TAP_TERM 120
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 
 #define EE_HANDS
 
